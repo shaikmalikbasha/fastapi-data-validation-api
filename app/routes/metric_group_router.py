dba094d95ce47881922e8803e621506ad0709787
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2022-05-15 18:38:20 +0530

    MdMetricGroup CRUD completed

--- app/routes/metric_group_router.py
+++ app/routes/metric_group_router.py
@@ -11,7 +11,6 @@ metric_group_service = MdMetricGroupService()
 
 @metric_group_router.get("/groups")
 async def get_all_metric_groups(db: Session = Depends(get_db)):
-    print("Getting the available groups")
     return metric_group_service.find_all_metric_groups(db)
 
 
@@ -20,3 +19,22 @@ async def save_metric_group(
     input_body: CreateAndUpdateMetricGroup, db: Session = Depends(get_db)
 ):
     return metric_group_service.save_metric_group(input_body.dict(), db)
+
+
+@metric_group_router.get("/groups/{group_name}")
+async def get_all_metric_groups(group_name: str, db: Session = Depends(get_db)):
+    return metric_group_service.find_metric_group_by_group_name(group_name, db)
+
+
+@metric_group_router.put("/groups/{group_id}")
+async def get_all_metric_groups(
+    group_id: int, input_body: CreateAndUpdateMetricGroup, db: Session = Depends(get_db)
+):
+    return metric_group_service.update_metric_group_by_group_id(
+        group_id, input_body.dict(), db
+    )
+
+
+@metric_group_router.delete("/groups/{group_id}")
+async def get_all_metric_groups(group_id: int, db: Session = Depends(get_db)):
+    return metric_group_service.delete_metric_group_by_group_id(group_id, db)
--- app/services/metric_group_service.py
+++ app/services/metric_group_service.py
@@ -1,4 +1,7 @@
+from tokenize import group
+
 from app.models.models import MdMetricGroup
+from fastapi import HTTPException, status
 
 
 class MdMetricGroupService:
@@ -18,8 +21,75 @@ class MdMetricGroupService:
         return new_group
 
     def find_all_metric_groups(self, db):
-        print("MdMetricGroups fetching..")
         stmt = db.query(MdMetricGroup).filter_by(is_active=True)
-        print(stmt)
+        print(f"SQL QUERY: {stmt}")
         groups = stmt.all()
+        print(f"MdMetricGroupResultSet: {groups}")
         return groups
+
+    def find_metric_group_by_group_name(self, group_name, db):
+        query = (
+            db.query(MdMetricGroup)
+            .filter_by(md_metric_group_name=group_name)
+            .filter_by(is_active=True)
+        )
+        print(f"SQL QUERY: {query}")
+        group = query.first()
+        if not group:
+            desc = {
+                "msg": "Invalid Input",
+                "description": f"Group with name: {group_name} was not found.",
+            }
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=desc)
+
+        return group
+
+    def find_metric_group_by_group_id(self, group_id, db):
+        query = db.query(MdMetricGroup).filter_by(id=group_id).filter_by(is_active=True)
+        print(f"SQL QUERY: {query}")
+        group = query.first()
+        if not group:
+            desc = {
+                "msg": "Invalid Input",
+                "description": f"Group with name: {group_id} was not found.",
+            }
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=desc)
+
+        return group
+
+    def update_metric_group_by_group_id(self, group_id, input_body, db):
+        query = db.query(MdMetricGroup).filter_by(id=group_id).filter_by(is_active=True)
+        print(f"SQL QUERY: {query}")
+
+        group = query.first()
+        if not group:
+            desc = {
+                "msg": "Invalid Input",
+                "description": f"Group with id: {group_id} was not found.",
+            }
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=desc)
+
+        for key, value in input_body.items():
+            setattr(group, key, value)
+
+        db.add(group)
+        db.commit()
+        db.refresh(group)
+
+        return group
+
+    def delete_metric_group_by_group_id(self, group_id, db):
+        query = db.query(MdMetricGroup).filter_by(id=group_id).filter_by(is_active=True)
+        print(f"SQL QUERY: {query}")
+
+        group = query.first()
+        if not group:
+            desc = {
+                "msg": "Invalid Input",
+                "description": f"Group with id: {group_id} was not found.",
+            }
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=desc)
+        db.delete(group)
+        db.commit()
+
+        return "OK"
